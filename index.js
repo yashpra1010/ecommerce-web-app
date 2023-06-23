c179c3bfb1b6132a9913df57a97f5dfe940410eb
Yash Prajapati <workwidyash@gmail.com>
2023-06-23 16:04:22 +0530

    CRUD - PCategory

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 const authRouter = require("./routes/authRoute.js");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/prodcategoryRoute");
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require("cookie-parser");
 dbConnect();
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use('/api/user', authRouter);
 app.use('/api/product', productRouter);
 app.use('/api/blog', blogRouter);
+app.use('/api/category', categoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
