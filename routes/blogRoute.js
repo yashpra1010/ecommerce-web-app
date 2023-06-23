ffad03b143beace4b75e8eba74c6a43ace56f1a5
Yash Prajapati <workwidyash@gmail.com>
2023-06-23 15:26:42 +0530

    CRUD Blog

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,8 +1,35 @@
 const express = require("express");
-const router = express.Router();
+const {
+	createBlog,
+	updateBlog,
+	getBlog,
+	getAllBlogs,
+	deleteBlog,
+	liketheBlog,
+	disliketheBlog,
+	uploadImages,
+} = require("../controller/blogCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
-const { createBlog, updateBlog } = require("../controller/blogCtrl");
+const { blogImgResize, uploadPhoto } = require("../middlewares/uploadImage");
+const router = express.Router();
 
-router.post("/",authMiddleware, isAdmin, createBlog);
+router.post("/", authMiddleware, isAdmin, createBlog);
+router.put(
+	"/upload/:id",
+	authMiddleware,
+	isAdmin,
+	uploadPhoto.array("images", 2),
+	blogImgResize,
+	uploadImages
+);
+router.put("/likes", authMiddleware, liketheBlog);
+router.put("/dislikes", authMiddleware, disliketheBlog);
+
+router.put("/:id", authMiddleware, isAdmin, updateBlog);
+
+router.get("/:id", getBlog);
+router.get("/", getAllBlogs);
+
+router.delete("/:id", authMiddleware, isAdmin, deleteBlog);
 
 module.exports = router;
